dfd58c5bb7197b645d9f19b1da7e25ed9840e2d9
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2024-12-16 20:25:37 -0300

    html semantico

--- src/Layouts/HomeLayout/index.tsx
+++ src/Layouts/HomeLayout/index.tsx
@@ -5,33 +5,36 @@ export function HomeLayout() {
     return (
         <div className="container mx-auto py-4">
             <div className="flex flex-col">
-                <div className="flex flex-row justify-between items-center justify-center">
-                    <div className="font-semibold text-xl">Home</div>
+                <header className="flex flex-row justify-between items-center justify-center">
+                    <h2 className="font-semibold text-2xl">Stock Manager System</h2>
 
-                    <NavigationMenu>
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <NavigationMenuTrigger>Produtos</NavigationMenuTrigger>
-                                <NavigationMenuContent>
-                                    <div className="bg-zinc-300 p-2 truncate">
-                                        Cradastre e Gerencie seus produtos
-                                    </div>
-                                </NavigationMenuContent>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <NavigationMenuTrigger>Localizações</NavigationMenuTrigger>
-                                <NavigationMenuContent>
-                                    <div className="bg-zinc-300 p-2 truncate">
-                                        Cradastre e Gerencie suas Localizações
-                                    </div>
-                                </NavigationMenuContent>
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenu>
-                </div>
-                <div className="bg-slate-200 flex flex-col items-center justify-center flex-wrap h-40 my-4 rounded-lg">
+                    <nav>
+                        <NavigationMenu>
+                            <NavigationMenuList>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger className="text-1xl">Produtos</NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        <div className="bg-zinc-300 p-2 truncate">
+                                            Cradastre e Gerencie seus produtos
+                                        </div>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger className="text-1xl">Localizações</NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        <div className="bg-zinc-300 p-2 truncate">
+                                            Cradastre e Gerencie suas Localizações
+                                        </div>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                            </NavigationMenuList>
+                        </NavigationMenu>
+                    </nav>
+                </header>
+                
+                <main className="bg-slate-200 flex flex-col items-center justify-center flex-wrap h-40 my-4 rounded-lg">
                     <Outlet />
-                </div>
+                </main>
             </div>
         </div>
     )
