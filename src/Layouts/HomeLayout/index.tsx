657b68ae493bc82b141ac1e596b6b1a5a735316f
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2024-12-16 20:12:26 -0300

    fazendo a navbar

--- src/Layouts/HomeLayout/index.tsx
+++ src/Layouts/HomeLayout/index.tsx
@@ -1,12 +1,38 @@
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@Components/ui/navigation-menu";
 import { Outlet } from "react-router";
 
 export function HomeLayout() {
     return (
-        <>
-            <h1 className="font-serif text-teal-800 text-lg">Texto em todas as rotas</h1>
-            <div className="bg-slate-200 flex flex-col items-center justify-center flex-wrap h-40 m-4 rounded-lg">
-                <Outlet />
+        <div className="container mx-auto py-4">
+            <div className="flex flex-col">
+                <div className="flex flex-row justify-between items-center justify-center">
+                    <div className="font-semibold text-xl">Home</div>
+
+                    <NavigationMenu>
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger>Produtos</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <div className="bg-zinc-300 p-2 truncate">
+                                        Cradastre e Gerencie seus produtos
+                                    </div>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger>Localizações</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <div className="bg-zinc-300 p-2 truncate">
+                                        Cradastre e Gerencie suas Localizações
+                                    </div>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </div>
+                <div className="bg-slate-200 flex flex-col items-center justify-center flex-wrap h-40 my-4 rounded-lg">
+                    <Outlet />
+                </div>
             </div>
-        </>
+        </div>
     )
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
-import { Home } from "@/pages/Home";
 import { HomeLayout } from "@/Layouts/HomeLayout";
-import { Perfil } from "@/pages/Perfil";
+import { Home } from "@Pages/Home";
+import { Perfil } from "@Pages/Perfil";
 import { Routes, Route } from "react-router";
 
 export function MyRoutes() {
