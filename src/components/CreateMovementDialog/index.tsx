ccde486864998a4131833077ad3ab215e07aab40
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2024-12-20 17:05:45 -0300

    teste do select no form

--- src/components/CreateMovementDialog/index.tsx
+++ src/components/CreateMovementDialog/index.tsx
@@ -1,24 +1,26 @@
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@Components/ui/dialog";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@Components/ui/dialog";
 import { DialogProps } from "@radix-ui/react-dialog";
 import { useForm } from "react-hook-form";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@Components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@Components/ui/form";
 import { Input } from "@Components/ui/input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createMovementSchema } from "@Utils/validation/movement";
+import { Button } from "@Components/ui/button";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@Components/ui/select";
     
 
 export function CreateMovementDialog ({...Props}: DialogProps) {
     const form = useForm<z.infer<typeof createMovementSchema>>({
         resolver: zodResolver(createMovementSchema),
         defaultValues: {
-            product: 1
+            product: '1'
         }
     })
 
     function onSubmit(data: z.infer<typeof createMovementSchema>) {
+        console.log('dan');
         console.log(data);
-        
     }
 
     return (
@@ -26,25 +28,51 @@ export function CreateMovementDialog ({...Props}: DialogProps) {
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Criar uma Movimentação</DialogTitle>
-                    <DialogDescription>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis </DialogDescription>
+                    <DialogDescription>Preença os dados abaixo:</DialogDescription>
                 </DialogHeader>
                     <div>
                         <Form {...form}>
                             <form onSubmit={form.handleSubmit(onSubmit)}>
-                                <FormField 
-                                    control={form.control}
-                                    name="product"
-                                    render={({field})=>(
-                                        <FormItem>
-                                            <FormLabel>Produto</FormLabel>
-                                            <FormControl>
-                                                <Input placeholder="Selecione o produto" {...field}/>
-                                            </FormControl>
-                                            <FormDescription>Produto da Movimentação</FormDescription>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
+                                <div className="grid grid-cols-2 gap-2">
+                                        <FormField 
+                                            control={form.control}
+                                            name="product"
+                                            render={({field})=>(
+                                                <FormItem>
+                                                    <FormLabel>Produto</FormLabel>
+                                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                                            <FormControl>
+                                                                <SelectTrigger>
+                                                                    <SelectValue placeholder="Escolha um produto" />
+                                                                </SelectTrigger>
+                                                            </FormControl>
+                                                            <SelectContent>
+                                                                <SelectItem value="1">
+                                                                    cotonete
+                                                                </SelectItem>
+                                                                <SelectItem value="2">
+                                                                    cotonete
+                                                                </SelectItem>
+                                                                <SelectItem value="3">
+                                                                    cotonete
+                                                                </SelectItem>
+                                                            </SelectContent>
+                                                        </Select>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                </div>
+                                <div className="flex justify-end gap-2">
+                                    <DialogClose>
+                                        <Button variant="destructive">
+                                            Cancelar
+                                        </Button>
+                                    </DialogClose>
+                                    <Button type="submit">
+                                        Criar
+                                    </Button>
+                                </div>
                             </form>
                         </Form>
                     </div>
--- src/utils/validation/movement.ts
+++ src/utils/validation/movement.ts
@@ -2,9 +2,7 @@
 import { z } from 'zod'
 
 export const createMovementSchema = z.object({
-    product: z.number({
-        message: "meu fi coloque um número pfv"
-    }).positive({
-        message: "meu fi coloque um número positivo pfv"
+    product: z.string({
+        required_error: 'Por favor selecione um produto'
     })
 })
