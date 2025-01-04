8e95b08ebda5c5e454c40e667712c576ee5a7a0c
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-01-04 13:25:33 -0300

    arrumando a organização da navbar

--- src/Layouts/HomeLayout/index.tsx
+++ src/Layouts/HomeLayout/index.tsx
@@ -5,40 +5,44 @@ import { Link, Outlet } from "react-router";
 
 export function HomeLayout() {
     return (
-        <div className="container mx-auto py-4">
+        <>
             <div className="flex flex-col">
-                <header className="flex flex-row justify-between items-center content-center sticky">
-                    <h2 className="font-semibold text-2xl capitalize">Stock Manager System</h2>
+                <header className="sticky py-4 top-0 bg-white">
+                    <div className="container mx-auto flex flex-row justify-between items-center content-center">
+                        <h2 className="font-semibold text-2xl capitalize">Stock Manager System</h2>
 
-                    <nav>
-                        <NavigationMenu>
-                            <NavigationMenuList>
-                                <NavigationMenuItem>
-                                    <Link to='/Products'>
-                                        <NavigationMenuLink className="text-1xl px-4 py-2 rounded-md font-semibold hover:bg-slate-100 capitalize">Produtos</NavigationMenuLink>
-                                    </Link>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem>
-                                    <Link to='/Locations'>
-                                        <NavigationMenuLink className="text-1xl px-4 py-2 rounded-md font-semibold hover:bg-slate-100 capitalize">Localizações</NavigationMenuLink>
-                                    </Link>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem>
-                                    <Link to='/'>
-                                        <NavigationMenuLink className="text-1xl px-4 py-2 rounded-md font-semibold hover:bg-slate-100 capitalize">Movimentações</NavigationMenuLink>
-                                    </Link>
-                                </NavigationMenuItem>
-                            </NavigationMenuList>
-                        </NavigationMenu>
-                    </nav>
+                        <nav>
+                            <NavigationMenu>
+                                <NavigationMenuList>
+                                    <NavigationMenuItem>
+                                        <Link to='/Products'>
+                                            <NavigationMenuLink className="text-1xl px-4 py-2 rounded-md font-semibold hover:bg-slate-100 capitalize">Produtos</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <Link to='/Locations'>
+                                            <NavigationMenuLink className="text-1xl px-4 py-2 rounded-md font-semibold hover:bg-slate-100 capitalize">Localizações</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <Link to='/'>
+                                            <NavigationMenuLink className="text-1xl px-4 py-2 rounded-md font-semibold hover:bg-slate-100 capitalize">Movimentações</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                </NavigationMenuList>
+                            </NavigationMenu>
+                        </nav>
+                    </div>
                 </header>
+
+
                 
-                <main className="bg-slate-100 my-4 p-2 rounded-lg">
+                <main className="bg-slate-100 my-4 container mx-auto p-2 rounded-lg">
                     <Outlet />
                 </main>
 
                 <Toaster />
             </div>
-        </div>
+        </>
     )
 }
