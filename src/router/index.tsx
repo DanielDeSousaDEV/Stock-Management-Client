04b7d8baf13e5e02605611ec3861f6d5b94a4884
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-08-03 19:44:20 -0300

    fix the imports

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,8 +5,8 @@ import { Products } from "@/pages/Products";
 import { Login } from "@/pages/Login";
 import { SignUp } from "@/pages/SignUp";
 import { ProtectedRoutes } from "@/components/ProtectedRoutes";
-import { LoginLayout } from "@/layouts/LoginLayout/index.tsx";
-import { HomeLayout } from "@/layouts/HomeLayout/index.tsx";
+import { LoginLayout } from "@/layouts/LoginLayout";
+import { HomeLayout } from "@/layouts/HomeLayout";
 
 export function MyRoutes() {
     return (
