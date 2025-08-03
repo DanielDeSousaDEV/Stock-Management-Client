2470b03a8a4364b2645ba70888d70300e10a1e10
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-08-03 20:11:18 -0300

    try resolve path errors

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,18 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias:{
-      ".": path.resolve(__dirname, "src")
+      "@": path.resolve(__dirname, "src")
     }
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+      
+      onwarn(warning, warn) {
+        // Exibe todos os avisos do Rollup na build
+        console.warn("[ROLLUP WARNING]", warning.message)
+      }
+    },
   }
 })
