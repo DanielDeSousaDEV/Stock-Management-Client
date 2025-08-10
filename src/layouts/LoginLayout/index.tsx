4146553eb8cf7163a28eecd1f3b0e2a9c4431c70
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-08-10 13:02:13 -0300

    adjusting code for deploy

--- src/layouts/LoginLayout/index.tsx
+++ src/layouts/LoginLayout/index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 
 export function LoginLayout() {
     return (
-        <div className="relative flex justify-center items-center min-h-screen w-screen bg-[url('@Assets/stockBg.jpg')] bg-center bg-no-repeat bg-cover">
+        <div className="relative flex justify-center items-center min-h-screen w-screen bg-[url('@/assets/stockBg.jpg')] bg-center bg-no-repeat bg-cover">
             <div className="absolute bg-black bg-opacity-20 backdrop-blur-sm min-h-screen w-screen"></div>
 
             <div className="z-10">
